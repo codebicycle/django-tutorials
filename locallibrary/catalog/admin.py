6a3c011d37cc6aff971d1bb72f2be892c7df12e5
codebicycle <codebicycle@gmail.com>
2017-10-11 10:39:54 +0300

    Customize admin detail view

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -12,7 +12,7 @@ admin.site.register(Language)
 
 class AuthorAdmin(admin.ModelAdmin):
 	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
+	fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 admin.site.register(Author, AuthorAdmin)
 
@@ -26,3 +26,7 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
 	list_display = ('book', 'status', 'due_back', 'id')
 	list_filter = ('status', 'due_back')
+	fieldsets = (
+		(None, {'fields': ('book', 'imprint', 'id')}),
+		('Availability', {'fields': ('status', 'due_back')}),
+	)
