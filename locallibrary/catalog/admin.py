45eac73a130d25ec0d54063baa9ebe955efa88cf
codebicycle <codebicycle@gmail.com>
2017-10-11 10:51:08 +0300

    Add inline editing of associated records

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -10,16 +10,29 @@ admin.site.register(Language)
 # admin.site.register(BookInstance)
 
 
+class BookInline(admin.TabularInline):
+	model = Book
+	extra = 0
+
+
 class AuthorAdmin(admin.ModelAdmin):
 	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 	fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+	inlines = [BookInline]
+
 
 admin.site.register(Author, AuthorAdmin)
+
+
+class BooksInstanceInline(admin.TabularInline):
+	model = BookInstance
+	extra = 0
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
 	list_display = ('title', 'author', 'display_genre')
+	inlines = [BooksInstanceInline]
 
 
 @admin.register(BookInstance)
