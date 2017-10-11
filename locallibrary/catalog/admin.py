ba7163627cb2b685e9561db74d3870869067dd76
codebicycle <codebicycle@gmail.com>
2017-10-11 10:25:53 +0300

    Add list filter to BookInstanceAdmin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -25,3 +25,4 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
 	list_display = ('book', 'status', 'due_back', 'id')
+	list_filter = ('status', 'due_back')
