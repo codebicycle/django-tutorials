01558b6e14bbda9f6fd5c4458f41abb4c648d31a
codebicycle <codebicycle@gmail.com>
2017-10-11 10:16:45 +0300

    Customize Book admin list view

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -19,7 +19,7 @@ admin.site.register(Author, AuthorAdmin)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('title', 'author', 'display_genre')
 
 
 @admin.register(BookInstance)
